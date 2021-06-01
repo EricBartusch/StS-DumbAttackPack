1ad92c41d7818b75da8c62136a46cb36bde3af60
Eric Bartusch <ericbartusch8@gmail.com>
2021-05-31 21:53:21 -0500

    Better balance

--- dumbAttackPack/src/main/java/dumbAttackPack/cards/OrbStrike.java
+++ dumbAttackPack/src/main/java/dumbAttackPack/cards/OrbStrike.java
@@ -8,7 +8,6 @@ import com.megacrit.cardcrawl.characters.AbstractPlayer;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.orbs.AbstractOrb;
-import com.megacrit.cardcrawl.orbs.EmptyOrbSlot;
 
 import static com.megacrit.cardcrawl.cards.red.PerfectedStrike.*;
 import static dumbAttackPack.DumbAttackPack.makeID;
@@ -16,14 +15,14 @@ import static dumbAttackPack.DumbAttackPack.makeID;
 public class OrbStrike extends AbstractEasyCard {
 
     /*
-     * (Channel 1 random orb.) Deal 10 damage. Deals additional damage for ALL your cards containing \"Strike\" times channeled orbs.
+     * (Channel 1 random orb.) Deal 10 damage. Deals additional damage for ALL your cards containing \"Strike\" plus orbs channeled this combat.
      */
 
     public static final String ID = makeID("OrbStrike");
 
     public OrbStrike() {
         super(ID, 2, CardType.ATTACK, CardRarity.COMMON, CardTarget.ENEMY, CardColor.BLUE);
-        baseDamage = 4;
+        baseDamage = 10;
         this.tags.add(CardTags.STRIKE);
     }
 
@@ -44,7 +43,10 @@ public class OrbStrike extends AbstractEasyCard {
 
     public void calculateCardDamage(AbstractMonster mo) {
         int realBaseDamage = this.baseDamage;
-        this.baseDamage += countChanneledOrbs() * countCards();
+        if (upgraded) { //account for orb to be channeled.
+            baseDamage++;
+        }
+        this.baseDamage += AbstractDungeon.actionManager.orbsChanneledThisCombat.size() + countCards();
         super.calculateCardDamage(mo);
         this.baseDamage = realBaseDamage;
         this.isDamageModified = this.damage != this.baseDamage;
@@ -52,24 +54,9 @@ public class OrbStrike extends AbstractEasyCard {
 
     public void applyPowers() {
         int realBaseDamage = this.baseDamage;
-        this.baseDamage += countChanneledOrbs() * countCards();
+        this.baseDamage += AbstractDungeon.actionManager.orbsChanneledThisCombat.size() +  countCards();
         super.applyPowers();
         this.baseDamage = realBaseDamage;
         this.isDamageModified = this.damage != this.baseDamage;
-    }
-
-    private int countChanneledOrbs() {
-        int orbs = 0;
-        for(int i = 0; i < AbstractDungeon.player.orbs.size(); ++i) {
-            if (!(AbstractDungeon.player.orbs.get(i) instanceof EmptyOrbSlot)) {
-                orbs++;
-            }
-        }
-
-        // Manually add newly channeled orb so preview damage on hover will be accurate
-        if (upgraded && AbstractDungeon.player.hasEmptyOrb()) {
-            orbs++;
-        }
-        return orbs;
     }
 }
